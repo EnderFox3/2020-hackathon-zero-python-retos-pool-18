f7f9337f75ef270ad67bb4fa27c7237ab51ad9b1
Carlos Luces <ender.fox3@gmail.com>
2020-05-16 16:57:48 +0200

    prueba

---
+++ src/kata1/rps.py
@@ -0,0 +1,28 @@
+from random import randint
+
+options = ["Piedra", "Papel", "Tijeras"]
+
+# El resultado de salida son las siguientes String
+#'Empate!'
+#'Ganaste!'
+#'Perdiste!'
+def quienGana(player, ai):
+    return "hola"
+
+# Entry Point
+def Game():
+    #
+    #
+    ai=options[randint(0,3)]
+
+    #
+    #
+    
+    winner = quienGana(player, ai)
+
+    print(winner)
+
+
+player=input("Seleciona entre: Piedra, Papel o Tijera: ")
+
+Game()
